0d453bea763f882f3f9c10ed540efecc0f69bf37
Abir-Bouhriz <bouhrizabir2015@gmail.com>
2020-07-19 11:35:55 +0100

    start

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
-$('.search-button').click(function(){
-    $(this).parent().toggleClass('open');
-  });
+//$('.search-button').click(function(){
+  //  $(this).parent().toggleClass('open');
+  //});
 
-function getConfirmation() {
-var result = confirm("Do you really want to precede your order ?");
-if (result == true) {
-    alert("Thanks for your confident ");
-}
-else {
-    alert("check out our new arrival collection ");
-}
-}
+//function getConfirmation() {
+//var result = confirm("Do you really want to precede your order ?");
+//if (result == true) {
+  //  alert("Thanks for your confident ");
+//}
+//else {
+  //  alert("check out our new arrival collection ");
+//}
+//}
 
+
+const iconMenu = document.querySelector('.menu-toggler');
+const menu = document.querySelector('.menu');
+
+iconMenu.addEventListener('click', function(){
+    this.classList.toggle('active');
+    menu.classList.toggle('active');
+})
